af3af9e523c9ec8dec652c80b81f5a37b238e9a9
Mert POLAT <24mertpolat@gmail.com>
2025-02-23 02:49:27 +0300

    Update-2

--- ProductCatolog-Core/Models/Cart.cs
+++ ProductCatolog-Core/Models/Cart.cs
@@ -12,7 +12,6 @@ namespace ProductCatolog_Core.Models
         public int Id { get; set; }
         public string UserId { get; set; }
         public DateTime CreatedDate { get; set; }
-
         public virtual IList<CartLine> CartLines { get; set; } = new List<CartLine>();
     }
 }
